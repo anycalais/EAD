9b15931a0eb8521cd20091305c96e8aae7ee619b
Kauany Calais <kauanycalais10@gmail.com>
2025-07-15 13:51:16 -0300

    atividade

---
+++ EAD/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using NomeDoProjeto.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    )
+);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
